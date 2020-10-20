5e8fb6b438cac1846c97a1f19d923d2961e6e1e1
Error4ik <fearerror@yandex.ru>
2020-10-20 21:57:25 +0300

    Palindrome number.

--- src/com/company/leetcode/palindrome_number/Solution.java
+++ src/com/company/leetcode/palindrome_number/Solution.java
@@ -18,5 +18,14 @@ public class Solution {
         }
 
         return true;
+//
+//        if (x < 0)
+//            return false;
+//        int num = 0, orig = x;
+//        while (x > 0) {
+//            num = (num * 10) + (x % 10);
+//            x = x / 10;
+//        }
+//        return orig == num;
     }
 }
