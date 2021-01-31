3adf29bed424d95545eb10741f46803c5ab16600
Error4ik <fearerror@yandex.ru>
2021-01-31 17:27:56 +0300

    Single Number.

---
+++ src/com/company/leetcode/single_number/Solution.java
@@ -0,0 +1,18 @@
+package com.company.leetcode.single_number;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            if (!list.contains(num)) {
+                list.add(num);
+            } else {
+                list.remove(new Integer(num));
+            }
+        }
+        return list.get(0);
+    }
+}
