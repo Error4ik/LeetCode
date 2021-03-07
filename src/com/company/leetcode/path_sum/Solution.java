7cbeca08c2a5a576e29867b277c3ebd5eebec457
Error4ik <fearerror@yandex.ru>
2021-03-07 19:38:34 +0300

    Path sum.

---
+++ src/com/company/leetcode/path_sum/Solution.java
@@ -0,0 +1,21 @@
+package com.company.leetcode.path_sum;
+
+public class Solution {
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        return this.hasSum(root, 0, targetSum);
+    }
+
+    private boolean hasSum(TreeNode node, int currentSum, int targetSum) {
+        if (node == null) {
+            return false;
+        }
+
+        currentSum += node.val;
+        if (node.left == null && node.right == null) {
+            return currentSum == targetSum;
+        }
+
+        return hasSum(node.left, currentSum, targetSum) || hasSum(node.right, currentSum, targetSum);
+    }
+}
