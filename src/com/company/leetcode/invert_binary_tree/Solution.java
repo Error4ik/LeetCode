ab4c4f3fff0db10d1a7ca3f49abe91b943872d1b
Error4ik <fearerror@yandex.ru>
2021-03-22 21:49:04 +0300

    Invert Binary tree.

---
+++ src/com/company/leetcode/invert_binary_tree/Solution.java
@@ -0,0 +1,15 @@
+package com.company.leetcode.invert_binary_tree;
+
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.right = left;
+        root.left = right;
+
+        return root;
+    }
+}
