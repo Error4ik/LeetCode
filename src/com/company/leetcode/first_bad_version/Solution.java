1d2ecd9a0f9d461ce2de075041c225fb950c213c
Error4ik <fearerror@yandex.ru>
2021-04-04 17:17:34 +0300

    First bad version

---
+++ src/com/company/leetcode/first_bad_version/Solution.java
@@ -0,0 +1,20 @@
+package com.company.leetcode.first_bad_version;
+
+import com.company.leetcode.Main.VersionControl;
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int start = 1;
+        int end = n;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (isBadVersion(mid)) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
