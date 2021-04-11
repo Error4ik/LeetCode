136b909582e7eecbbe8aaa545759e7074cf1c583
Error4ik <fearerror@yandex.ru>
2021-04-11 22:13:37 +0300

    Power of four

---
+++ src/com/company/leetcode/power_of_four/Solution.java
@@ -0,0 +1,19 @@
+package com.company.leetcode.power_of_four;
+
+public class Solution {
+    public boolean isPowerOfFour(int n) {
+        while (n > 0) {
+            if (n == 1) {
+                return true;
+            }
+
+            if (n % 4 != 0) {
+                return false;
+            }
+
+            n = n / 4;
+        }
+
+        return false;
+    }
+}
